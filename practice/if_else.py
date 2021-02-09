a7f3c9164125e02a61718f9c9d52cbd97a86a795
dj255 <dhruvikjha@gmail.com>
2021-02-09 02:07:48 +0000

    Q1 and Q2

---
+++ practice/if_else.py
@@ -0,0 +1,9 @@
+x = str(input("Who sees me rolling? (cops, wardens, other)"))
+
+
+if x == "cops":
+    print("They see me rolling. They hating. They hoping they gonna catch me riding dirty!")
+elif x == "wardens":
+    print("My music's so loud. I'm swanging. They hoping they gonna catch me riding dirty!")
+else:
+    print("Trying to catch me riding dirty!")
--- practice/main.py
+++ practice/main.py
@@ -1,1 +1,4 @@
-
+print("Who broke my heart?")
+x = input()
+print("My First Love ," + x, ", broke my heart for the first time.")
+print("And I was like baby, baby, baby oh.")
